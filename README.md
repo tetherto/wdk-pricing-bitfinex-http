3be4fd5bf1e136529591c9862de004d6b6df11df
George Javakhidze <g.javakhidze@noxtton.com>
2024-12-31 12:33:00 +0100

    feat: version 0.0.1

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# lib-wallet-pricing-bitfinex-http
+# Lib Wallet Pricing - Bitfinex HTTP
+
+This library is an implementation of [lib-wallet-pricing-provider](https://github.com/tetherto/lib-wallet-pricing-bitfinex-http) `PricingClient`. It exposes methods to obtain the current price & historical data for given ticker.
+
+## Installation
+
+1. Install the required dependencies:
+
+```bash
+npm install lib-wallet-pricing-bitfinex-http
+```
+
+## Usage
+
+```js
+// Initialise the client
+const client = new BitfinexPricingClient();
+
+// Get latest price for BTCUSD/
+const currentPrice = await client.getCurrentPrice('BTC', 'USD');
+
+// Get Hitorical price. If results are higher than MAX_HISTORICAL_ENTRIES (100), the response will be downscaled by x2
+const historicalPrice = await client.getHistoricalPrice({
+  from: 'BTC',
+  to: 'USD',
+  start: 1709906400000, // Optional, Start date for historical interval
+  end: 1709913600000, // Optional, End date for historical interval
+});
+```
---
+++ index.js
@@ -0,0 +1,89 @@
+'use strict'
+
+import axios from 'axios'
+import { PricingClient } from 'wdk-lib-pricing'
+
+export class BitfinexPricingClient extends PricingClient {
+  HISTORICAL_DATA_AGE = 365 * 24 * 60 * 60000
+  MAX_HISTORICAL_ENTRIES = 100
+
+  /**
+   * Creates a new BitfinexPricingClient instance.
+   */
+  constructor () {
+    super()
+    this.client = axios.create({
+      baseURL: 'https://api-pub.bitfinex.com/v2'
+    })
+  }
+
+  /**
+   * Returns the current price of the asset pair
+   * @async
+   * @param {string} from
+   * @param {string} to
+   * @returns {Promise<number>}
+   */
+  async getCurrentPrice (from, to) {
+    const response = await this.client.get(`/ticker/t${from}${to}`)
+
+    return response.data[6]
+  }
+
+  /**
+   * Returns the recorded lowest ask (not trade) of the asset pair.
+   * Bitfinex only supports max 250 records per request.
+   * @async
+   * @param {HistoricalPriceOptions} opts
+   * @returns {Promise<HistoricalPriceResult[]>}
+   */
+  async getHistoricalPrice (opts) {
+    if (opts.start && opts.start < new Date().getTime() - this.HISTORICAL_DATA_AGE) {
+      throw new Error('Start date should be within last 365 days')
+    }
+
+    const start = opts?.start
+    const end = opts?.end
+
+    const results = []
+
+    let cursor = end
+
+    // Bitfixes returns data rounded to 1 hour && results are always in descending order
+    while (Math.abs(cursor - start) > 3600000) {
+      const response = await this.client.get(
+        `/tickers/hist?symbols=t${opts.from}${opts.to}&limit=100&start=${start}&end=${cursor}`
+      )
+
+      if (!response.data.length) {
+        break
+      }
+
+      results.push(
+        ...response.data.map((item) => ({
+          price: item[3],
+          ts: item[12]
+        }))
+      )
+
+      const resultStart = response.data[response.data.length - 1][12]
+
+      cursor = resultStart
+    }
+
+    return this._cappedToMaxResults(results)
+  }
+
+  /**
+   * Cuts the results to the maximum number of entries.
+   * @param {HistoricalPriceResult[]} results
+   * @returns {HistoricalPriceResult[]}
+   */
+  _cappedToMaxResults (results) {
+    if (results.length <= this.MAX_HISTORICAL_ENTRIES) {
+      return results
+    }
+
+    return this._cappedToMaxResults(results.filter((_, index) => index % 2 === 0))
+  }
+}
