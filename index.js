2ee1c4b78ddc550ab5eb9929dfef0df6368534c6
dmytro-herko116 <Dmytro.Herko@devoxsoftware.com>
2026-02-18 13:41:53 +0100

    Fixed linter

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict'
 // Copyright 2024 Tether Operations Limited
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -14,8 +14,8 @@
 // limitations under the License.
 //
 
-import { PricingClient } from "@tetherto/wdk-pricing-provider";
-import axios from "axios";
+import { PricingClient } from '@tetherto/wdk-pricing-provider'
+import axios from 'axios'
 
 /**
  * @typedef {Object} CurrencyPair
@@ -39,17 +39,17 @@ import axios from "axios";
 
 export class BitfinexPricingClient extends PricingClient {
   /** @internal */
-  HISTORICAL_DATA_AGE = 365 * 24 * 60 * 60000;
+  HISTORICAL_DATA_AGE = 365 * 24 * 60 * 60000
 
   /** @internal */
-  MAX_HISTORICAL_ENTRIES = 100;
+  MAX_HISTORICAL_ENTRIES = 100
 
-  constructor() {
-    super();
+  constructor () {
+    super()
     /** @internal */
     this.client = axios.create({
-      baseURL: "https://api-pub.bitfinex.com/v2",
-    });
+      baseURL: 'https://api-pub.bitfinex.com/v2'
+    })
   }
 
   /**
@@ -57,90 +57,90 @@ export class BitfinexPricingClient extends PricingClient {
    * @param {string} to - Quote currency (e.g. 'USD')
    * @returns {Promise<number>}
    */
-  async getCurrentPrice(from, to) {
+  async getCurrentPrice (from, to) {
     const response = await this.client.post(
-      "/calc/fx",
+      '/calc/fx',
       {
         ccy1: from.toUpperCase(),
-        ccy2: to.toUpperCase(),
+        ccy2: to.toUpperCase()
       },
       {
         headers: {
-          contentType: "application/json",
-          accept: "application/json",
-        },
-      },
-    );
-    return response.data[0];
+          contentType: 'application/json',
+          accept: 'application/json'
+        }
+      }
+    )
+    return response.data[0]
   }
 
   /**
    * @param {CurrencyPair[]} pairs - Array of currency pairs
    * @returns {Promise<number[]>} Array of prices in the same order as input pairs
    */
-  async getMultiCurrentPrice(pairs) {
+  async getMultiCurrentPrice (pairs) {
     const symbols = pairs
       .map((p) => `t${p.from.toUpperCase()}${p.to.toUpperCase()}`)
-      .join(",");
+      .join(',')
 
-    const response = await this.client.get(`/tickers?symbols=${symbols}`);
+    const response = await this.client.get(`/tickers?symbols=${symbols}`)
 
-    const SYMBOL_INDEX = 0;
-    const LAST_PRICE_INDEX = 7;
-    const priceBySymbol = new Map();
+    const SYMBOL_INDEX = 0
+    const LAST_PRICE_INDEX = 7
+    const priceBySymbol = new Map()
 
     for (const ticker of response.data) {
-      priceBySymbol.set(ticker[SYMBOL_INDEX], ticker[LAST_PRICE_INDEX]);
+      priceBySymbol.set(ticker[SYMBOL_INDEX], ticker[LAST_PRICE_INDEX])
     }
 
     return pairs.map((p) => {
-      const symbol = `t${p.from.toUpperCase()}${p.to.toUpperCase()}`;
-      return priceBySymbol.get(symbol);
-    });
+      const symbol = `t${p.from.toUpperCase()}${p.to.toUpperCase()}`
+      return priceBySymbol.get(symbol)
+    })
   }
 
   /**
    * @param {HistoricalPriceOptions} opts
    * @returns {Promise<HistoricalPriceResult[]>}
    */
-  async getHistoricalPrice(opts) {
+  async getHistoricalPrice (opts) {
     if (
       opts.start &&
       opts.start < new Date().getTime() - this.HISTORICAL_DATA_AGE
     ) {
-      throw new Error("Start date should be within last 365 days");
+      throw new Error('Start date should be within last 365 days')
     }
 
-    const start = opts?.start;
-    const end = opts?.end;
+    const start = opts?.start
+    const end = opts?.end
 
-    const results = [];
+    const results = []
 
-    let cursor = end;
+    let cursor = end
 
     // Bitfinex returns data rounded to 1 hour, results are always in descending order
     while (Math.abs(cursor - start) > 3600000) {
       const response = await this.client.get(
-        `/tickers/hist?symbols=t${opts.from}${opts.to}&limit=100&start=${start}&end=${cursor}`,
-      );
+        `/tickers/hist?symbols=t${opts.from}${opts.to}&limit=100&start=${start}&end=${cursor}`
+      )
 
       if (!response.data.length) {
-        break;
+        break
       }
 
       results.push(
         ...response.data.map((item) => ({
           price: item[3],
-          ts: item[12],
-        })),
-      );
+          ts: item[12]
+        }))
+      )
 
-      const resultStart = response.data[response.data.length - 1][12];
+      const resultStart = response.data[response.data.length - 1][12]
 
-      cursor = resultStart;
+      cursor = resultStart
     }
 
-    return this._cappedToMaxResults(results);
+    return this._cappedToMaxResults(results)
   }
 
   /**
@@ -148,13 +148,13 @@ export class BitfinexPricingClient extends PricingClient {
    * @param {HistoricalPriceResult[]} results
    * @returns {HistoricalPriceResult[]}
    */
-  _cappedToMaxResults(results) {
+  _cappedToMaxResults (results) {
     if (results.length <= this.MAX_HISTORICAL_ENTRIES) {
-      return results;
+      return results
     }
 
     return this._cappedToMaxResults(
-      results.filter((_, index) => index % 2 === 0),
-    );
+      results.filter((_, index) => index % 2 === 0)
+    )
   }
 }
